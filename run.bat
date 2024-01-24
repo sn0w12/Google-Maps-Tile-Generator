73999e3afb6fe071ecb9de491a7e14b9e3bbcea6
sn0w12 <lucasskold49@gmail.com>
2024-01-24 01:16:22 +0100

    Added dependency check for easier running.

--- run.bat
+++ run.bat
@@ -1,5 +1,20 @@
 @echo off
+setlocal enabledelayedexpansion
 CALL "C:\ProgramData\anaconda3\Scripts\activate.bat"
+
+python check_dependencies.py > missing_packages.txt
+SET DEPENDENCY_CHECK=%ERRORLEVEL%
+IF !DEPENDENCY_CHECK! EQU 1 (
+    echo Missing packages found.
+    set /p UserResponse=Do you want to install missing packages? [Y/n]: 
+    IF /I "!UserResponse!"=="Y" (
+        echo Installing missing packages...
+        for /F "tokens=*" %%i in (missing_packages.txt) do pip install %%i
+    ) ELSE (
+        echo Skipping package installation.
+        exit
+    )
+)
 
 echo Please enter the full path to your input image:
 set /p inputImagePath="Input image path: "
